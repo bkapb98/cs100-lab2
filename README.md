220ba6b769f91bb2b2e0521e758310f5bbaf5f76
Ben <bkaplan@college.harvard.edu>
2018-09-23 10:53:25 -0400

    finished

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Lab 2
 
-Collaboration between Eliza Scharfstein & Ben Kaplan
+Collaboration between Eliza Scharfstein & Ben Kaplan. 
+
+The index page is essentially unchanged from the distribution code, and the gallery page works quite similarly. 
+
+The object page is similar in many ways, but we do some prior filtering/modification of values (people and comments) prior to rendering the HMTL. Specifically, we populate the people array with a blank string to avoid errors if there are no people associated with that object. Within the objects HTML itself, we iterated through all people for display. Additionally, we iterate through the comments so that only those comments for that specific object are passed in to be rendered on the page. Within the artifact HTML itself (we called the file artifact to distinguish from objects), we iterated through each comment for display.
+
+We used a get request and harnessed req.query for adding comments because it was far simpler (for technical implementation) than using a post request. This design decision does mean you have to press back twice to get back to the gallery's page (or even might not work sometimes), but we thought this small sacrifice was worth it given the advantages of using this process of commenting, simplicity of implementation being the key advantage. 
+
+Lastly, we switched from using object id to object number as the api call appear to be failing to the Harvard Arts Museum or was returning the wrong data.
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 require('cross-fetch/polyfill');
 
+// initialize universal constants 
 const app = express();
 const host = '127.0.0.1';
-const port = 3000;
-
+const port = 8000;
+let comments = [];
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 const API_KEY = "743ce3f0-b77e-11e8-bf0e-e9322ccde4db";
 
 // behavior for the index route
@@ -21,7 +21,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/gallery/:gallery_id', function(req, res) {
+// behavior for gallery route
+app.get('/gallery/:gallery_id', (req, res) => {
   const galleryId = req.params.gallery_id;
   console.log(galleryId);
   const url = `https://api.harvardartmuseums.org/object?size=100&apikey=${API_KEY}&gallery=${galleryId}`;
@@ -33,16 +34,36 @@ app.get('/gallery/:gallery_id', function(req, res) {
   });
 });
 
-app.get('/object/:object_id', function (req, res) {
-    const objectId = req.params.object_id;
-    console.log(objectId);
-    const url = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&object=${objectId}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.records);
-            res.render('artifact', { object: data.records[0] });
-      });
+// behavior for object route
+app.get('/object/:object_id', (req, res) => {
+  const objectId = req.params.object_id;
+  const url = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&objectnumber=${objectId}`;
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          console.log(data.records);
+          // add a blank string to people to avoid error if none exist
+          if (!data.records[0].people) {
+            data.records[0].people = [''];
+          }
+          
+          // parses through all comments to return only relevant ones
+          let object_comments = []; 
+          comments.forEach(thought => {
+            if (thought.id == objectId) { 
+              object_comments.push(thought); 
+            }
+          });
+          res.render('artifact', { object: data.records[0], comments: object_comments })
+      });
+});
+
+// behavior for adding comments 
+app.get('/add_comment/:objectId', (req, res) => { 
+  const objectId = req.params.objectId;
+  const newComment = req.query.comments;
+  comments.push({id: objectId, text: newComment});
+  res.redirect(`/object/${objectId}`);
 });
 
 app.listen(port, host, () => {
